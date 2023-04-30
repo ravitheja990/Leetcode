31fcb829f22c8b1e4c763f9371da879da0ec653e
ravitheja990 <ravitheja990@gmail.com>
2023-04-30 19:41:39 +0530

    700. Search in a Binary Search Tree

---
+++ Search in a Binary Search Tree/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node5 = new TreeNode(3);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+
+        Solution solution = new Solution();
+        System.out.println("ele 2 is in this BST node: " + solution.searchBST(node1, 2));
+        System.out.println("ele 12 is in this BST node: " + solution.searchBST(node1, 12));
+    }
+}
