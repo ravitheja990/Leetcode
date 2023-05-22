22849ef51175811643cba14d71006ae042112a8f
ravitheja990 <ravitheja990@gmail.com>
2023-05-22 18:50:05 +0530

    2481. Minimum Cuts to Divide a Circle

---
+++ Minimum_Cuts_to_Divide_a_Circle/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        Solution solution = new Solution();
+        System.out.println("number of cuts needed to get given equal cuts is :: " + solution.numberOfCuts(n));
+    }
+}
