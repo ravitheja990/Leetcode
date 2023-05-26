a044cf9e5e1cc64083f26dcbda0251ee967210b9
ravitheja990 <ravitheja990@gmail.com>
2023-05-26 20:30:57 +0530

    876. Middle of the Linked List

---
+++ Middle_of_the_Linked_List/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+        ListNode node8 = new ListNode(8);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+
+        Solution solution = new Solution();
+        System.out.println("middle node value is :: " + solution.middleNode(node1).val);
+    }
+}
